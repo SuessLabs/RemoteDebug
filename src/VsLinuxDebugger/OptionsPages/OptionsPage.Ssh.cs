ea79ee86d69139afd2003c23fa6078b783ad48c5
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2024-11-10 11:22:30 +0100

    Added setting that allows to change the remote X11 display number

--- src/VsLinuxDebugger/OptionsPages/OptionsPage.Ssh.cs
+++ src/VsLinuxDebugger/OptionsPages/OptionsPage.Ssh.cs
@@ -7,9 +7,10 @@ namespace Xeno.VsLinuxDebug.OptionsPages
 {
   public partial class OptionsPage : DialogPage
   {
-    private const string Credientials = "Remote Credientials";
+    private const string Credentials = "Remote Credentials";
+    private const string XDisplay = "Remote X11 Display";
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("Host IP Address")]
     [Description("Host IP Address. On VMs using 'NAT', set IP to '127.0.0.1' and forward Port 22. PCs and VMs 'Bridged', have their own IP.")]
     public string HostIp { get; set; } = "127.0.0.1";
@@ -19,39 +20,39 @@ namespace Xeno.VsLinuxDebug.OptionsPages
     ////[Description("Remote Host Port Number (SSH Default is 22)")]
     public int HostPort { get; set; } = 22;
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("User Group Name (optional)")]
     [Description("Remote Machine Group Name. For basic setups (i.e. RaspberryPI) it's the same as UserName.")]
     public string UserGroupName { get; set; } = "";
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("User Name")]
     [Description("SSH User name on remote machine.")]
     public string UserName { get; set; } = "pi";
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("User Password")]
     [Description("SSH Password on remote machine.")]
     public string UserPass { get; set; } = "raspberry";
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("SSH Key File Enabled")]
     [Description("Use SSH Key for connecting to remote machine.")]
     public bool UserPrivateKeyEnabled { get; set; } = false;
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("SSH Private Key File (optional)")]
     [Description("Private key file.")]
     public string UserPrivateKeyPath { get; set; } = Path.Combine(
       Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
       ".ssh\\id_rsa");
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("SSH Private Key Password (optional)")]
     [Description("Private key password (only if it was set).")]
     public string UserPrivateKeyPassword { get; set; } = "";
 
-    [Category(Credientials)]
+    [Category(Credentials)]
     [DisplayName("Use SSH.exe with integrated user/[..]/.ssh/id_rsa instead of PLINK")]
     [Description("Use SSH.exe with integrated user/[..]/.ssh/id_rsa instead of PLINK")]
     public bool UseSSHExeEnabled { get; set; } = true;
@@ -68,5 +69,10 @@ namespace Xeno.VsLinuxDebug.OptionsPages
       Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
       ".ssh\\id_rsa");
     */
+
+    [Category(XDisplay)]
+    [DisplayName("Remote X11 Display Number (optional)")]
+    [Description("Remote X11 Display number (only if it was set). Defaults to ':0'.")]
+    public string RemoteDebugDisplayNumber { get; set; } = ":0";
   }
 }
